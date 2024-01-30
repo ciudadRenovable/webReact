b745e48cb717669f81f68434596df1c04d3c46c0
Jose Manuel Osorio <jm157653@gmail.com>
2024-01-30 13:17:59 -0500

    add tag to projects

--- src/components/CarouselProjects/index.jsx
+++ src/components/CarouselProjects/index.jsx
@@ -1,6 +1,6 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Autoplay } from 'swiper/modules';
-import { AutoPlay } from 'swiper';
+import { Autoplay } from 'swiper/modules';
+// import { AutoPlay } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import './carouselProjects.css';
@@ -8,88 +8,28 @@ import './carouselProjects.css';
 
 export const CarouselProjects = () => {
   return (
-    <>
-
-      <div className='flex flex flex-col lg:flex-row gap-5 flex-wrap md:flex-nowrap'>
-        <div className="card">
-          <img src="/images/projects/tiendasAra.png" alt="" />
-          <div className='info_type_project'>
-            Industrial
-          </div>
-          <h2>Tiendas ARA</h2>
+    <div className='flex flex flex-col lg:flex-row gap-5 flex-wrap md:flex-nowrap'>
+      <a className="card" href='/proyectos/Mundial%20Granos%20y%20Panelas'>
+        <img src="https://i.postimg.cc/524r936k/Mundial-Gy-P1-29-11zon.webp" style={{ borderRadius: '20px' }} alt="proyecto" />
+        <div className='info_type_project'>
+          Industrial
         </div>
-        <div className="card">
-          <img src="/images/projects/tronexVitelsa.png" alt="" />
-          <div className='info_type_project'>
-            Industrial
-          </div>
-          <h2>Tronex Vitelsa</h2>
+        <h2>Mundial Granos y Panelas</h2>
+      </a>
+      <a className="card" href='/proyectos/Tronex'>
+        <img src="/images/projects/tronexVitelsa.png" alt="proyecto" />
+        <div className='info_type_project'>
+          Industrial
         </div>
-        <div className="card">
-          <img src="/images/projects/yokoMotor.png" alt="" />
-          <div className='info_type_project'>
-            Industrial
-          </div>
-          <h2>Yokomotor</h2>
+        <h2>Tronex Vitelsa</h2>
+      </a>
+      <a className="card" href='/proyectos/Yokomotor'>
+        <img src="/images/projects/yokoMotor.png" alt="proyecto" />
+        <div className='info_type_project'>
+          Industrial
         </div>
-      </div>
-      {/* <Swiper
-        slidesPerView={1}
-        spaceBetween={20}
-        slidesPerGroup={1}
-        loop={true}
-        navigation={false}
-        modules={[Autoplay]}
-        className="mySwiper"
-        autoplay={{
-          delay: 1500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        breakpoints={{
-          640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          1400: {
-            slidesPerView: 3,
-            spaceBetween: 50,
-          },
-        }}
-      >
-        <SwiperSlide>
-          <div className="card">
-            <img src="./images/project.png" alt="" />
-            <h2>Proyecto 01</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-              ligula eget dolor.
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="card">
-            <img src="./images/project.png" alt="" />
-            <h2>Proyecto 01</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-              ligula eget dolor.
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="card">
-            <img src="./images/project.png" alt="" />
-            <h2>Proyecto 01</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-              ligula eget dolor.
-            </p>
-          </div>
-        </SwiperSlide>
-      </Swiper> */}
-    </>
+        <h2>Yokomotor</h2>
+      </a>
+    </div>
   )
-} 
+}
